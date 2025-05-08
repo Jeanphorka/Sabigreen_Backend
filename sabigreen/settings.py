208bf058047e80a875c0ac7cdb86e3b85faf7018
Jeanphorka <jeanphorka@gmail.com>
2025-05-09 02:44:11 +0700

    fix: correct STATIC_URL path and indentation for STATICFILES_STORAGE in settings

--- sabigreen/settings.py
+++ sabigreen/settings.py
@@ -121,12 +121,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 if not DEBUG:
     STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # เพิ่มสำหรับใช้ WhiteNoise ใน production
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
