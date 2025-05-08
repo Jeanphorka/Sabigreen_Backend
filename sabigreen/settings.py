510fc90159ee62ce4887b9ad1ad14e8c68ea5a21
Jeanphorka <jeanphorka@gmail.com>
2025-05-09 02:46:40 +0700

    fix: adjust STATIC_ROOT and STATICFILES_STORAGE settings for proper static file handling

--- sabigreen/settings.py
+++ sabigreen/settings.py
@@ -122,11 +122,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = '/static/'
-if not DEBUG:
-    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # เพิ่มสำหรับใช้ WhiteNoise ใน production
-    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
