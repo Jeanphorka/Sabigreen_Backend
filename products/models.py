0fbe0d00a1df6890722d314a48a7db995d799582
Jeanphorka <jeanphorka@gmail.com>
2025-05-08 22:31:34 +0700

    feat: add product

--- products/models.py
+++ products/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-class TeaType(models.Model):
+class Type(models.Model):
     name = models.CharField(max_length=100, unique=True)
 
     def __str__(self):
@@ -8,11 +8,11 @@ class TeaType(models.Model):
 
 class Product(models.Model):
     name = models.CharField(max_length=200)
-    weight_grams = models.PositiveIntegerField()  # น้ำหนักหน่วยกรัม เช่น 500
-    price_thb = models.DecimalField(max_digits=10, decimal_places=2)  # เช่น 750.00
+    weight_grams = models.PositiveIntegerField(null=True, blank=True)  
+    price_thb = models.DecimalField(max_digits=10, decimal_places=2)  
     description = models.TextField()
-    tea_type = models.ForeignKey(TeaType, on_delete=models.SET_NULL, null=True, blank=True)
-    in_stock = models.PositiveIntegerField(default=0)  # จำนวนสินค้าคงเหลือ
+    type = models.ForeignKey(Type, on_delete=models.SET_NULL, null=True, blank=True)
+    in_stock = models.PositiveIntegerField(default=0)  
     image = models.ImageField(upload_to='products/' , null=True, blank=True)
 
     def __str__(self):
--- products/serializers.py
+++ products/serializers.py
@@ -2,7 +2,7 @@ from rest_framework import serializers
 from .models import Product
 
 class ProductSerializer(serializers.ModelSerializer):
-    tea_type = serializers.StringRelatedField()  # ให้แสดงชื่อแทน id
+    type = serializers.StringRelatedField()  # ให้แสดงชื่อแทน id
 
     class Meta:
         model = Product
